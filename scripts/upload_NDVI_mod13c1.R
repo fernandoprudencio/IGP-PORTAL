b4c74f6219e55de4ff3a603578b1481c336a3d7c
fernando-desktop <fjprudenciop@gmail.com>
2020-09-20 15:54:36 -0500

    script to upload NDVI and anomalies of Amazon basin were added

---
+++ scripts/upload_NDVI_mod13c1.R
@@ -0,0 +1,63 @@
+#' @title
+#' Plot the spatial distribution of GVMI an GVMI anomalies
+#'
+#' @description
+#' this script plots, as monitoring, the spatial distribution of GMVI and GVMI
+#'   anomalies for each month of the current year
+#'
+#' @author Fernando Prudencio
+rm(list = ls())
+
+#' INSTALL PACKAGES
+pkg <- c("RCurl")
+
+sapply(
+  pkg,
+  function(x) {
+    is.there <- x %in% rownames(installed.packages())
+    if (is.there == FALSE) {
+      install.packages(x, dependencies = T)
+    }
+  }
+)
+
+#' LOAD PACKAGES
+library(RCurl)
+
+#' UPLOAD TO LAMAR SERVER
+path <- "/data/users/lamar/DATA/FIG_WEB_LAMAR/PROJECTS/ANA/NDVI"
+host <- "181.177.244.92"
+nam1 <- "ndvi"
+nam2 <- "anom_ndvi"
+year <- "2020"
+month <- "08"
+
+ftpUpload(
+  sprintf("exports/%1s.png", nam1),
+  sprintf("ftp://%1$s/%2$s/%3$s.png", host, path, nam1),
+  userpwd = "amazonia:l@mar1044"
+)
+
+ftpUpload(
+  sprintf("exports/%1s.png", nam2),
+  sprintf("ftp://%1$s/%2$s/%3$s.png", host, path, nam2),
+  userpwd = "amazonia:l@mar1044"
+)
+
+ftpUpload(
+  sprintf("exports/%1s.png", nam1),
+  sprintf(
+    "ftp://%1$s/%2$s/2020/%5$s/%3$s%4$s_%5$s.png",
+    host, path, year, month, nam1
+  ),
+  userpwd = "amazonia:l@mar1044"
+)
+
+ftpUpload(
+  sprintf("exports/%1s.png", nam2),
+  sprintf(
+    "ftp://%1$s/%2$s/2020/%5$s/%3$s%4$s_%5$s.png",
+    host, path, year, month, nam2
+  ),
+  userpwd = "amazonia:l@mar1044"
+)
